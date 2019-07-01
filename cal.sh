0a77a468e0a795abfc9a2a9a7fa2390d5e8c9970
xcovery45 <48662920+xcovery45@users.noreply.github.com>
2019-07-01 11:56:37 +0700

    Add files via upload

---
+++ cal.sh
@@ -0,0 +1,11 @@
+clear
+toilet -f slant --metal "Calender"
+sleep 1
+echo "\033[33;1mAuthor: ZxC7"
+sleep 0.03
+echo "\033[33;1mTool biar lu g lupa hari"
+echo
+sleep 1
+echo "\033[31;1[====CALENDER====]"
+echo "\033[36;1m"
+python2 calenc.py
